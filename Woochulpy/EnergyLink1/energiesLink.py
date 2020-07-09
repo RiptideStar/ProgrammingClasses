385c38b046ca7680088305ad4c12b31741b3ac61
Kyle Z <kyle1001001@gmail.com>
2020-07-09 16:14:37 -0700

    Final Touches on the EnergyLink Program

--- Woochulpy/EnergyLink1/energiesLink.py
+++ Woochulpy/EnergyLink1/energiesLink.py
@@ -1,3 +1,9 @@
+"""
+Chemistry: A program designed to link energy values to a constant number of data points.
+A energy txt file and a data txt file are used.
+By: Kyle Zhang
+"""
+
 filePath = "Woochulpy/EnergyLink1/" #leave blank if not necessary
 #file names
 energyFileName = filePath + "ener.txt"
@@ -10,25 +16,25 @@ indexToStartSorting = 0  #set to zero if the whole list isn't sorted, and set to
 #creation of a class to link energy values with data
 class EnergyData:
     def __init__(self, energy):
-        #instatiating class with an energy and corresponding array of integer dataPerEnergy length/size
+        #instantiating class with an energy and corresponding array of integer dataPerEnergy length/size
         self.energy = energy
         self.data = [0] * dataPerEnergy
 
-list = [] #declaration of our list of Energy Values
+listEnergyData = [] #declaration of our list of Energy Values
 linesEnergy = open(energyFileName).read().splitlines() #read the file every line-by-line into this list
 for line in linesEnergy:
-    list.append( EnergyData(line) ) #append each line into the energy field of the corresponding energy into a list of energy classes
+    listEnergyData.append( EnergyData(line) ) #append each line into the energy field of the corresponding energy into a list of energy classes
 
 #CheckPoints
 print("Program: Appended Energy Values")
 
 linesNumbers = open(numbersFileName).read().splitlines() #read the file every line-by-line into this list
 #Appending the data values to their corresponding energy values in order
-for i in range(len(list)):
+for i in range(len(listEnergyData)):
     for j in range(dataPerEnergy):
         #this index expression represents the index of the overall nmr.txt line in terms of i and j, which are indices for the "2d array"
         index = i * dataPerEnergy + j
-        list[i].data[j] = linesNumbers[index]
+        listEnergyData[i].data[j] = linesNumbers[index]
 
 #CheckPoints
 print("Program: Appended Data Values")
@@ -39,19 +45,19 @@ I need to move it to the front of the list, and start circulating again for ever
 I keep dragging the most minimum number out in front of the previous minimum number until I reach the last number, which evidently is the greatest,
 so it is sorted from most negative to max. """
 #we sort every index but the last one, since once sorted, the last index is bound to be the greatest
-for i in range(indexToStartSorting, len(list)-1):
+for i in range(indexToStartSorting, len(listEnergyData)-1):
     #declaring my minimum to the first index
-    min = float(list[i].energy)
+    min = float(listEnergyData[i].energy)
     minIndex = i
     #searching everything after the first index and comparing to see if it is less than it, if it is, it becomes new minimum to be compared against
-    for i1 in range(i+1, len(list)): #named i1 because it circulates through every index 1 above i
-        if (float(list[i1].energy) < min):
-            min = float(list[i1].energy)
+    for i1 in range(i+1, len(listEnergyData)): #named i1 because it circulates through every index 1 above i
+        if (float(listEnergyData[i1].energy) < min):
+            min = float(listEnergyData[i1].energy)
             minIndex = i1
     #remove Energy value at where it was and insert at front of list where it is last sorted
-    temp = list[minIndex]
-    list.remove(list[minIndex])
-    list.insert(i, temp)
+    temp = listEnergyData[minIndex]
+    listEnergyData.remove(listEnergyData[minIndex])
+    listEnergyData.insert(i, temp)
 
 #CheckPoints
 print("Program: Sorted the List of Energy Values")
@@ -61,7 +67,7 @@ def outputNLinesToFile(n, list):
     #The File is doesn't have to exist, as it will create or overwrite a file. User feel free to change the file name or path for output
     fileName = filePath + "lowest_" + n + "data.txt" 
 
-    #The + sign means it will overwrite if exists, and create if doesn't
+    #The w+ means it will overwrite if exists, and create if doesn't
     outputFile = open(fileName, "w+")
     #Output everything starting from least energy value's data values up until desired energy level
     for energyIndex in range(int(n)):
@@ -73,7 +79,7 @@ response = "y"
 #loop to always keep getting information until user doesn't want any
 while (response != "n"):
     inp = input("How many energy values (sorted lowest to highest) would you like to see data for (typing zero creates an empty txt file, just don't)? ")
-    outputNLinesToFile(inp, list)
+    outputNLinesToFile(inp, listEnergyData)
     print("File created:", "lowest_" + inp + "data.txt")
     resp = input("Would you like to create another one of these files? (y/n): ")
     response = resp.lower()
